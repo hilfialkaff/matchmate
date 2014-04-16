a69c9d7622ffa901eb37587e7ea42b334beb4746
Joseph Ciurej <ciurej2@illinois.edu>
2014-04-15 21:28:10 -0500

    Updated the scheme for the question JSON items and added more functionality to the question rendering functions.

---
+++ static/js/matchmate-data.js
@@ -0,0 +1,23 @@
+/**
+ * @file matchbar-data.js
+ * @author Joseph Ciurej
+ * @date Spring 2014
+ *
+ * Container Module for Test Matchbar Data
+ *
+ * @TODO
+ * - Add more contants to this module as needed.
+ */
+
+/// Module Constants ///
+
+/**
+ * A constant matchbar JSON object that represents an intermediate matchbar that
+ * represents the matchbar for a person halfway completed with the survey.
+ */
+var TEST_MATCHBAR_INT = [
+	{ "qid": 1, "category": 3, "name": "Words of Affirm 1", "val": true, "weight": 1 },
+	{ "qid": 2, "category": 3, "name": "Words of Affirm 2", "val": false, "weight": 4 },
+	{ "qid": 6, "category": 2, "name": "Acts of Service 1", "val": true, "weight": 2 },
+];
+
--- static/js/matchmate-draw.js
+++ static/js/matchmate-draw.js
@@ -6,14 +6,52 @@
  * Container Module for All `MatchMate` Drawing Functions
  *
  * @TODO
- * - Write the implementation for this script!
+ * - Add functionality to the rendering to add padding between matchslit
+ *   rectangles.
  */
 
 /// Module Constants ///
 
+/**
+ * The maximum number of slits that will be contained in a given match bar rendered.
+ * This number corresponds to the number of questions.
+ */
+var MAX_MATCHSLIT_COUNT = 25;
 
 
 /// Module Functions ///
+
+// Helper Functions //
+
+/**
+ * Given the numerical identifier for a category, this function returns the
+ * string for this category.
+ */
+function getCategoryString( _categoryNumber )
+{
+	var catid2catstring = { 0 : "qt", 1 : "pt", 2 : "woa", 3 : "rg", 4 : "aos" };
+	return catid2catstring[ _categoryNumber ];
+}
+
+/**
+ * Given the JSON object representing a particular match slit, this function
+ * returns the HTML element identifier for that slit.
+ */
+function getMatchslitID( _matchslitJSON )
+{
+	return "matchslit" + _matchslitJSON[ "qid" ];
+}
+
+/**
+ * Given the JSON object representing a particular match slit, this function
+ * returns the HTML classification for that slit.
+ */
+function getMatchslitClass( _matchslitJSON )
+{
+	return "matchslit " + getCategoryString( _matchslitJSON["category"] );
+}
+
+// Primary Functions //
 
 /**
  * Renders the match bar represented by the given JSON object to the page div
@@ -24,6 +62,26 @@
  */
 function renderMatchbar( _destDivID, _matchbarJSON )
 {
-	console.log( "Hello, world!" );
+	// Constant Values //
+	var cDestDivID = "#" + _destDivID;
+	var cDestDivWidth = $( cDestDivID ).width();
+	var cDestDivHeight = $( cDestDivID ).height();
+
+	var cMatchslitWidth = cDestDivWidth;
+	var cMatchslitHeight = Math.floor( cDestDivHeight / MAX_MATCHSLIT_COUNT );
+
+	// Functionality //
+	var destElement = d3.select( cDestDivID );
+	var matchslitElements = destElement.selectAll( "matchslit" )
+		.data( _matchbarJSON ).enter().append( "rect" )
+		.attr( "id", getMatchslitID )
+		.attr( "class", getMatchslitClass )
+		.attr( "x", 0 )
+		.attr( "y", 0 )
+		.attr( "width", cMatchslitWidth )
+		.attr( "height", cMatchslitHeight )
+		.attr( "rx", )
+		.attr( "ry", )
+		.attr( "stroke-width", );
 }
 
