c88be5bca720392a25bcfe5f66f3fb67c77a8d3f
Joseph Ciurej <ciurej2@illinois.edu>
2014-04-15 19:05:18 -0500

    Added a test data file for testing of the drawing functions.

---
+++ static/js/matchmate-draw.js
@@ -0,0 +1,29 @@
+/**
+ * @file matchmate-draw.js
+ * @author Joseph Ciurej
+ * @date Spring 2014
+ *
+ * Container Module for All `MatchMate` Drawing Functions
+ *
+ * @TODO
+ * - Write the implementation for this script!
+ */
+
+/// Module Constants ///
+
+
+
+/// Module Functions ///
+
+/**
+ * Renders the match bar represented by the given JSON object to the page div
+ * given by the supplied identifier.
+ *
+ * @param _destDivID The string identifier for the destination div.
+ * @param _matchbarJSON The JSON object representing the matchbar to be rendered.
+ */
+function renderMatchbar( _destDivID, _matchbarJSON )
+{
+	console.log( "Hello, world!" );
+}
+
