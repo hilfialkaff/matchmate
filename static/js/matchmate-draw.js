8e4d6888c4e552a2c0a686b07c739a217502e3e5
Joseph Ciurej <ciurej2@illinois.edu>
2014-04-15 21:48:25 -0500

    Refactored the global variables in the drawing module so that they're globally accessible.

--- static/js/matchmate-draw.js
+++ static/js/matchmate-draw.js
@@ -10,20 +10,6 @@
  *   rectangles.
  */
 
-/// Module Constants ///
-
-/**
- * The maximum number of slits that will be contained in a given match bar rendered.
- * This number corresponds to the number of questions.
- */
-var MAX_MATCHSLIT_COUNT = 25;
-
-/**
- * A dictionary containing the mapping from the question category IDs stored in
- * the database to question category strings.
- */
-var CATID2CATSTRING = { 0 : "qt", 1 : "pt", 2 : "woa", 3 : "rg", 4 : "aos" };
-
 /// Module Functions ///
 
 // Helper Functions //
---
+++ static/js/matchmate-globals.js
@@ -0,0 +1,24 @@
+/**
+ * @file matchmate-globals.js
+ * @author Efe Karakus, Joseph Ciurej
+ * @date Spring 2014
+ *
+ * Container Module for All Global Constants for MatchMate
+ *
+ * @TODO
+ */
+
+/// Module Constants ///
+
+/**
+ * The maximum number of slits that will be contained in a given match bar rendered.
+ * This number corresponds to the number of questions.
+ */
+var MAX_MATCHSLIT_COUNT = 25;
+
+/**
+ * A dictionary containing the mapping from the question category IDs stored in
+ * the database to question category strings.
+ */
+var CATID2CATSTRING = { 0 : "qt", 1 : "pt", 2 : "woa", 3 : "rg", 4 : "aos" };
+
